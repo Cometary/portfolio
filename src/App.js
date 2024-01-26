1d0df8c8399b0eea84db3359466e514ae8a0390e
Cometary <109892844+Cometary@users.noreply.github.com>
2024-01-26 19:48:49 +0900

    feat : layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Content from './content/Content';
+import Footer from './footer/Footer';
+import Header from './header/Header.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          portfolio
-        </a>
-      </header>
+      <Header/>
+      <Content/>
+      <Footer/>
     </div>
   );
 }
